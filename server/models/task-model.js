a5ddab61a852b069e380dcbf5636662d9a560406
unknown <heyauio@gmail.com>
2023-05-08 15:27:18 +0300

    добавил эндпоинты для задач

--- server/models/task-model.js
+++ server/models/task-model.js
@@ -9,7 +9,8 @@ const TaskSchema = new Schema({
     // functionName: {type: String, required: false}, // Поле для названия задачи
     // arguments: [{type: String}], // массив строк
     difficulty: {type: String, enum: ["easy", "medium", "hard"], required: false}, // Поле для сложности задачи
-    examples: [String] // массив строк
+    examples: [String], // массив строк с примерами решений
+    likes: [Number] // массив чисел для хранения id пользователей, лайкнувших задачу
     // solutions: {} // массив добавить новую модель для решений [SolutionModel]
 });
 
